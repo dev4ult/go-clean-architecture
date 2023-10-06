7c94e0a03584845fd089619c9cf1d53f08913f1e
dev4ult <nibrasbiasaaja@gmail.com>
2023-10-06 13:40:02 +0700

    feat: setup server and db connection in env

--- config/connection.go
+++ config/connection.go
@@ -2,14 +2,30 @@ package config
 
 import (
 	user "clean_arch/features/user/entity"
+	"fmt"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type Config struct {
+	SERVER_PORT int
+	DB_USER 	string
+	DB_PASS 	string
+	DB_PORT     string
+	DB_HOST     string
+	DB_NAME     string
+	SECRET      string
+}
+
 func InitDB() *gorm.DB {
 
-	dsn := "root@tcp(127.0.0.1:3306)/try_middleware?charset=utf8mb4&parseTime=True&loc=Local"
+	config := loadConfig()
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DB_USER, config.DB_PASS, config.DB_HOST, config.DB_PORT, config.DB_NAME)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -17,6 +33,26 @@ func InitDB() *gorm.DB {
 	}
 	
 	return db
+}
+
+func loadConfig() Config {
+	godotenv.Load(".env")
+	
+	serverPort, err := strconv.Atoi(os.Getenv("SERVER"))
+
+	if err != nil {
+		panic(err)
+	}
+	
+	return Config{
+		SERVER_PORT: serverPort,
+		DB_USER: os.Getenv("DB_USER"),
+		DB_PASS: os.Getenv("DB_PASS"),
+		DB_PORT: os.Getenv("DB_PORT"),
+		DB_HOST: os.Getenv("DB_HOST"),
+		DB_NAME: os.Getenv("DB_NAME"),
+		SECRET:  os.Getenv("SECRET"),
+	}
 }
 
 func autoMigrate(db *gorm.DB) {
